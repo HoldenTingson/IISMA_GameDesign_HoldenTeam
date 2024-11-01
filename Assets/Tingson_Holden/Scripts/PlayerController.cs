9d90825379a4683d05147551b3dfd46b9ff906b6
HoldenTingson <holdentingson33@gmail.com>
2024-11-01 15:16:05 +0100

    Adding player physics, bush, tree, and monster slime object

--- Assets/Tingson_Holden/Scripts/PlayerController.cs
+++ Assets/Tingson_Holden/Scripts/PlayerController.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float _moveSpeed = 1f;
 
-    private PlayerControls playerControls;
-    private Vector2 movement;
-    private Rigidbody2D rb;
-    private Animator myAnimator;
-    private SpriteRenderer mySpriteRenderer;
+    private PlayerControls _playerControls;
+    private Vector2 _movement;
+    private Rigidbody2D _rb;
+    private Animator _myAnimator;
+    private SpriteRenderer _mySpriteRenderer;
 
     private void Awake()
     {
-        playerControls = new PlayerControls();
-        rb = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        _playerControls = new PlayerControls();
+        _rb = GetComponent<Rigidbody2D>();
+        _myAnimator = GetComponent<Animator>();
+        _mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
     {
-        playerControls.Enable();
+        _playerControls.Enable();
     }
 
     private void Update()
@@ -38,15 +38,15 @@ public class PlayerControl : MonoBehaviour
 
     private void PlayerInput()
     {
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
-        myAnimator.SetFloat("moveX", movement.x);
-        myAnimator.SetFloat("moveY", movement.y);
+        _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+        _myAnimator.SetFloat("moveX", _movement.x);
+        _myAnimator.SetFloat("moveY", _movement.y);
 
     }
 
     private void Move()
     {
-        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        _rb.MovePosition(_rb.position + _movement * (_moveSpeed * Time.fixedDeltaTime));
     }
 
 
@@ -56,11 +56,11 @@ public class PlayerControl : MonoBehaviour
 
         if (horizontalInput < 0)
         {
-            mySpriteRenderer.flipX = true;
+            _mySpriteRenderer.flipX = true;
         }
         else if (horizontalInput > 0)
         {
-            mySpriteRenderer.flipX = false;
+            _mySpriteRenderer.flipX = false;
         }
     }
 }
