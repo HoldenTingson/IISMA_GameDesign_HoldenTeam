151552ae7615f124a78235b97c356139fb616ed1
bimoaaryo <bimoa672@gmail.com>
2024-11-28 16:21:05 +0100

    Add: - Object: GameManager, Chest, Dialogue Box, TextChest - Script: ChestInteraction, Dialogue, EnemyManager

---
+++ Assets/Ariotedjo_Muhammad/Script/ChestInteraction.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ChestInteraction : MonoBehaviour
+{
+    [SerializeField] private Dialogue dialogue; // Reference to the Dialogue script
+
+    private void OnMouseDown()
+    {
+        if (EnemyManager.Instance.GetEnemyCount() > 0)
+        {
+            dialogue.StartDialogue();
+        }
+        else
+        {
+            Debug.Log("All enemies have been defeated. You may proceed.");
+        }
+    }
+}
---
+++ Assets/Ariotedjo_Muhammad/Script/Dialogue.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    private bool isDialogueActive = false;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        gameObject.SetActive(false); // Hide the dialogue box initially
+    }
+
+    void Update()
+    {
+        if (isDialogueActive && Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        index = 0;
+        isDialogueActive = true;
+        gameObject.SetActive(true); // Show the dialogue box
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+        gameObject.SetActive(false); // Hide the dialogue box
+    }
+}
