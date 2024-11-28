9efe4d1e13813f80dd23100e70d5666040ef7d26
HoldenTingson <holdentingson33@gmail.com>
2024-11-28 17:19:10 +0100

    Add pickup spawner and item, fix lightning and camera bug

---
+++ Assets/Tingson_Holden/Scripts/PickupSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _healthGlobe, _staminaGlobe;
+
+    public void DropItems()
+    {
+        int randomNum = Random.Range(1, 5);
+
+        if (randomNum == 1)
+        {
+            Instantiate(_healthGlobe, transform.position, Quaternion.identity);
+        }
+
+        if (randomNum == 2)
+        {
+            Instantiate(_staminaGlobe, transform.position, Quaternion.identity);
+        }
+    }
+}
